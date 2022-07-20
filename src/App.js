16e59e7b3a9608f834d263c6c96a34f8ce7c3f15
Charlie <59826303+linkin8288@users.noreply.github.com>
2022-07-20 09:36:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-    <Home />
+    
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/hotels" element={<List/>}/>
